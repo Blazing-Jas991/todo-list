7d31a110848d50604842391669dacad21b9f2b82
Blazing-Jas991 <223303023+Blazing-Jas991@users.noreply.github.com>
2026-04-17 01:20:29 -0700

    add common.js and seperate webpack files for webpack-merge

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import "./styles.css";
 import { greeting } from "./template.js";
 
+ if (process.env.NODE_ENV !== 'production') {
+   console.log('Looks like we are in development mode!');
+ }
+
+const button = document.querySelector('.butt');
+button.addEventListener('click', () => {
+    const para = document.createElement('p');
+    para.textContent = "Index.js is working fine";
+    document.body.appendChild(para)
+}) 
+
 console.log(greeting);
